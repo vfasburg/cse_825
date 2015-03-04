e81fb0d2693dccf529cc7ff00e7056e57dd8a1cf
Vince Fasburg <vfasburg@umich.edu>
2015-03-03 23:10:11 -0500

    rule now works on only HIDs and changes keyboard map immediately. Restricted keyboard updated with more restrictions

--- Project/linux_script/setup/usb_add.sh
+++ Project/linux_script/setup/usb_add.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-xmodmap /home/vince/Documents/Xmodmap_restricted
-#44 is the letter j
-#/user/bin/xmodmap -verbose -e 'keycode 44 = NoSymbol' > /usr/local/bin/text.txt 
 
-
+sudo -u vince xmodmap -display :0 /home/vince/Documents/Xmodmap_restricted &
+#echo "hello world" > /tmp/test.log
