8db75f589c33dbe48c687d5ab81a590afb546b3c
Vince Fasburg <vfasburg@umich.edu>
2015-03-20 23:57:04 -0400

    fixed script to allow running a jar from udev rule. Still has multiple instances issue

--- Project/linux_script/setup/usb_add.sh
+++ Project/linux_script/setup/usb_add.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#this line should be uncommented
+# sudo -u vince xmodmap -display :0 /home/vince/Documents/Xmodmap_restricted &
+xhost local:vince
+export DISPLAY=:0.0
+java -jar /home/vince/Downloads/ValidateUSB.jar
 
-sudo -u vince xmodmap -display :0 /home/vince/Documents/Xmodmap_restricted &
 #echo "hello world" > /tmp/test.log
