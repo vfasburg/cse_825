38e80592d9822a832bdfaaf4682fe7829e732944
Vince Fasburg <vfasburg@umich.edu>
2015-03-22 15:32:49 -0400

    now restricts keyboard and runs jar. Once jar is closed, it restores keyboard. commands to prevent mouse clicks are commented out. Those should be uncommented once a jar is avaiable that will close when the captcha is completed.

--- Project/linux_script/setup/usb_add.sh
+++ Project/linux_script/setup/usb_add.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 #this line should be uncommented
-# sudo -u vince xmodmap -display :0 /home/vince/Documents/Xmodmap_restricted &
-xhost local:vince
-export DISPLAY=:0.0
-java -jar /home/vince/Downloads/ValidateUSB.jar
+if [ -f /home/vince/.testfile ]
+then
+  :
+else
+  echo "" > /home/vince/.testfile
+  sudo -u vince xmodmap -display :0 /home/vince/Documents/Xmodmap_restricted #&
+  #sudo -u vince xmodmap -display :0 -e "pointer = 0 0 0 0 0 0 0 0 0 0 0 0 "
+  xhost local:vince
+  export DISPLAY=:0.0
+  java -jar /home/vince/Downloads/ValidateUSB.jar
+  #sleep 20
+  #sudo -u vince xmodmap -display :0 -e "pointer = 1 2 3 4 5 6 7 8 9 10 11 12 "
+  sudo -u vince xmodmap -display :0 /home/vince/Documents/Xmodmap_orig
+  rm -f /home/vince/.testfile
+fi
 
-#echo "hello world" > /tmp/test.log
+
