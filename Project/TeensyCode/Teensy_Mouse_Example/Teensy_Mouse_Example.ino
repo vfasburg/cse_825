809a16b07f075471e2bc74512b1b4ce07f5d26ef
Vince Fasburg <vfasburg@umich.edu>
2015-03-22 17:37:40 -0400

    added example of how mouse could be used to open terminal without any keyboard modifiers

---
+++ Project/TeensyCode/Teensy_Mouse_Example/Teensy_Mouse_Example.ino
@@ -0,0 +1,38 @@
+  
+void setup() {
+  Serial.begin(9600);
+  delay(1000);
+}
+
+void loop() {
+int i;
+for(i = 0; i < 15; i++) {
+  usb_mouse_move(-127,-127,0);
+}
+usb_mouse_move(30,60,0);
+
+usb_mouse_buttons(1,0,0);
+usb_mouse_buttons(0,0,0);
+
+delay(250);
+usb_keyboard_press(KEY_BACKSPACE, 0);
+
+usb_mouse_move(120,0,0);
+
+
+Keyboard.print("terminal");
+
+usb_mouse_move(0,110,10);
+delay(500);
+usb_mouse_buttons(1,0,0);
+usb_mouse_buttons(0,0,0);
+
+//usb_keyboard_press(KEY_ENTER, 0);
+
+delay(1200);
+
+Keyboard.print("echo \"any command I want\"\n");
+
+
+delay(300000);
+}
