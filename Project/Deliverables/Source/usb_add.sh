381f922057803eac57bbd3dfd4914ec7fceabb26
Bonnie R <breiff13@gmail.com>
2015-04-12 18:55:47 -0400

    Updated the usb_add script to have generic username in it.

--- Project/Deliverables/Source/usb_add.sh
+++ Project/Deliverables/Source/usb_add.sh
@@ -4,7 +4,7 @@
 # for Michigan State University, CSE 825
 
 # Script setup code
-xhost local:vince
+xhost local:<username>
 export DISPLAY=:0.0
 
 # Look for the hidden testfile that indicates whether the keyboard and mouse are currently restricted
@@ -15,13 +15,13 @@ else
   # Create the testfile indicator
   echo "" > /usr/local/bin/.testfile
   # Restrict the keyboard to only non-special keys and prevent the mouse from clicking
-  sudo -u vince xmodmap -display :0 -e "pointer = 0 0 0 0 0 0 0 0 0 0 0 0 "
-  sudo -u vince xmodmap -display :0 /usr/local/bin/Xmodmap_restricted
+  sudo -u <username> xmodmap -display :0 -e "pointer = 0 0 0 0 0 0 0 0 0 0 0 0 "
+  sudo -u <username> xmodmap -display :0 /usr/local/bin/Xmodmap_restricted
   # Run the human validation captcha
-  sudo -u vince java -jar /usr/local/bin/ValidateUSB_Hover.jar
+  sudo -u <username> java -jar /usr/local/bin/ValidateUSB_Hover.jar
   # Allow the full range of keyboard keys and allow the mouse to click 
-  sudo -u vince xmodmap -display :0 -e "pointer = 1 2 3 4 5 6 7 8 9 10 11 12 "
-  sudo -u vince xmodmap -display :0 /usr/local/bin/Xmodmap_orig
+  sudo -u <username> xmodmap -display :0 -e "pointer = 1 2 3 4 5 6 7 8 9 10 11 12 "
+  sudo -u <username> xmodmap -display :0 /usr/local/bin/Xmodmap_orig
   # Remove the testfile indicator
   rm -f /usr/local/bin/.testfile
 fi
